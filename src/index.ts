1d675e7a0b85d4cea7f59e2ade2c73e615c2c80d
David Petrásek <davidpetrasek@hotmail.cz>
2026-01-13 10:51:59 +0100

    fix naming

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,15 @@
 import {Manager} from './Manager';
 import {AjaxForm} from './AjaxForm';
-// import { cLog } from '@dpsys/js-utils/misc';
 
-const axFormsInstance = new Manager();
+const manager = new Manager();
 
-const axForm = (formOrSelector: HTMLFormElement | string): AjaxForm | null => {
-    return axFormsInstance.get(formOrSelector);
+const ajaxForm = (formOrSelector: HTMLFormElement | string): AjaxForm | null => {
+    return manager.get(formOrSelector);
 };
 
 /**
   * @deprecated Use ajaxForm(form) instead ajaxForm.get(form)
   */
-axForm.get = axFormsInstance.get.bind(axFormsInstance);
+ajaxForm.get = manager.get.bind(manager);
 
-// cLog('AjaxForm :: Module loaded', axForm);
-
-export default axForm;
-
+export default ajaxForm;
